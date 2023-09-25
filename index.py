cac702dc80f7e3879255164c25853e935f771951
LuizHenriqueDelPosoVilalva <luizgarciaevilalva@gmail.com>
2023-09-24 22:16:56 -0400

    atividade avaliatica concluida

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import datetime
 
 def menu_remedio (lista, cont):
+    lista_horarios = []
     print("--Gerenciamento de remedios--")
     while True:
         print("--Menu de opções--")
@@ -14,6 +15,10 @@ def menu_remedio (lista, cont):
         print("8 - Calcular a media dos valores dos remedios -")
         print("9 - Calcular a media dos remedios em estoque -")
         print("10 - Buscar remedio com maior valor")
+        print("11 - Buscar remedio de menor valor -")
+        print("12 - Buscar palavra dentro da descrição -")
+        print("13 - Listar horario de chamadas das funções")
+        print("14 - Ordenar os remedios em ordem crescente ou decrescente -")
         print("0 - Sair -")
         
         opcao = int(input(">> ")) 
@@ -21,49 +26,109 @@ def menu_remedio (lista, cont):
             case 1:
                 print("--Cadastrar remedio--")
                 cont = cadastrar(lista, cont)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função cadastrar foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 2:
                 print("--Listar remedios cadastrados--")
                 imprimir_remedios(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de listar foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 3:
                 print("-- Buscar pelo remedio --")
                 nome = input("Digite o nome do remedio: ")
                 posicao = buscar_posicao(lista, nome)
-                if posicao >= 0:
-                    print("Remedio encontrado")
-                    print(lista[posicao])
-                else:
-                    print("Remedio não encontrado")
+                print("Remedio encontrado")
+                print(lista[posicao])
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de buscar remedio foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 4:
                 print("Lista de remedio genericos")
                 buscar_generico(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de listar remedios genericos foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 5:
                 print("Lista de remedios não genericos")
                 buscar_nao_generico(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de listar remedios NÃO genericos foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 6:
                 print("Atualizar campos do remedio")
                 nome = input("Digite o nome do remedio que você deseje atualizar: ")
                 posicao = buscar_posicao(lista, nome)
-                if posicao >= 0:
-                    atualizar_campo(posicao, lista)
-                else:
-                    print("Digite um remedio cadastrado")
+                atualizar_campo(posicao, lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de atualizar campos do remedio foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 7:
                 print("--Remover remedio--")
                 nome = input("Digite o nome do remedio a ser excluido: ")
                 posicao = buscar_posicao(lista, nome)
-                if posicao >= 0:
-                    remover_remedio(lista, posicao)
-                else:
-                   print("Digite um remedio cadastrado")
+                remover_remedio(lista, posicao)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de remover remedio foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 8:
                 print("--Media dos valores do projeto--")
                 apresentar_media_valores(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de calcular media dos valores foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 9:
                 print("--Media de remedios em estoque--")
                 apresentar_media_quantidade(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de calcular media do estoque foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
             case 10:
                 print("--Maior valor--")
                 maior_valor(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de consulta do maior valor foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
+            case 11:
+                print("--Menor valor--")
+                menor_valor(lista)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de consulta do menor valor foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
+            case 12:
+                print("--Busca palavra--")
+                nome = input("Digite o nome do remedio que queira fazer a pesquisa em sua descrição: ")
+                palavra = input("Digite a palavra: ")
+                pos = buscar_posicao(lista, nome)
+                elemento = lista[pos]
+                buscar_palavra(elemento, palavra)
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de buscar palavra foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
+            case 13:
+                print(lista_horarios)
+            case 14:
+                print("ordenar pelo nome, faça sua escolha")
+                print("-----------------")
+                while True:
+                    print("1 - Para crescente")
+                    print("2 - Para decrescente")
+                    print("0 - sair")
+                    opcao = int(input(">> "))
+                    match opcao:
+                        case 1:
+                            ordenar_crescente(lista)
+                        case 2:
+                            ordenar_descrescente(lista)
+                        case 0:
+                            break
+                        case _:
+                            print("Opção invalida")
+                hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
+                apresentação = f"A função de ordenar pelo nome do remedio foi chamada as: {hora_atual}"
+                lista_horarios.append(apresentação)
+
             case 0:
                 break
             case  _:
@@ -87,7 +152,6 @@ def cadastrar(lista, cont):
                 break
     valor = float(input("Digite o valor do remedio: "))
     quantidade = int(input("Digite a quantidade em estoque: "))
-    hora_atual = datetime.datetime.now().strftime("%H:%M:%S")
     remedio = dict(
         id = id, 
         nome = nome, 
@@ -95,7 +159,6 @@ def cadastrar(lista, cont):
         generico = generico, 
         valor = valor, 
         quantidade = quantidade,
-        hora = hora_atual
     )
     lista.append(remedio)
     cont+=1
@@ -113,7 +176,6 @@ def modelo_de_apresentacao(item):
     print("Genérico: ", item['generico'])
     print("Valor do remedio: ", item['valor'])
     print("Quantidade em estoque: ", item['quantidade'])
-    print("Hora do cadastro: ", item['hora'])
     print("---------------------------------------")
 
 def buscar_posicao (lista, nome):
@@ -122,34 +184,30 @@ def buscar_posicao (lista, nome):
         for i in range(tamanho):
             if lista[i]['nome'] == nome:
                 return i
-            else:
-                return -1
-        else:
-            return -1
 
 def buscar_generico(lista):
-    print("-Lista de remedios genericos-")
     if (not lista):
          return "A lista de alunos está vazia."
 
-    for i in lista:
-        nome_do_remedio = i['nome']
-        generico = i['generico']
+    tamanho = len(lista)
+    for i in range(tamanho):
+        nome_do_remedio = lista[i]['nome']
+        generico = lista[i]['generico']
         if generico == True:
             apresentacao = f"{nome_do_remedio} é genérico"
-    print(apresentacao)
+            print(apresentacao)
 
 def buscar_nao_generico(lista):
-    print("-Lista de remedios não genericos-")
     if (not lista):
          return "A lista de alunos está vazia."
 
-    for i in lista:
-        nome_do_remedio = i['nome']
-        generico = i['generico']
+    tamanho = len(lista)
+    for i in range(tamanho):
+        nome_do_remedio = lista[i]['nome']
+        generico = lista[i]['generico']
         if generico == False:
-            resultado = f"{nome_do_remedio} não é generico"
-    print(resultado)
+            apresentacao = f"{nome_do_remedio} não é generico"
+            print(apresentacao)
 
 def atualizar_campo(pos,lista):
     print("Menu de opções de atualização")
@@ -234,8 +292,56 @@ def apresentar_media_quantidade (lista):
 
 def maior_valor (lista):
     tamanho = len(lista)
-    for i in range(0, lista):
-        for j in range()
+    for i in range(0, tamanho):
+        for j in range(0, 1):
+            if lista[i]['valor'] < lista[j]['valor']:
+                temp = lista[i]
+                lista[i] = lista[j]
+                lista[j] = temp
+    apresentação = f"o remedio de maior valor é o {lista[i]['nome']} com o valor de {lista[i]['valor']}"
+    print(apresentação)
+
+def menor_valor(lista):
+    tamanho = len(lista)
+    for i in range(0, tamanho):
+        for j in range(0, 1):
+            if lista[i]['valor'] > lista[j]['valor']:
+                temp = lista[i]
+                lista[i] = lista[j]
+                lista[j] = temp
+    apresentação = f"o remedio de menor valor é o {lista[i]['nome']} com o valor de {lista[i]['valor']}"
+    print(apresentação)
+
+def buscar_palavra(elemento, palavra):
+    lista_descricao = elemento['descricao']
+    palavras = lista_descricao.split()
+    if palavra in palavras:
+        resultado = f"A palavra {palavra} está contida na descrição '{lista_descricao}'"
+    else:
+        resultado = f"A palavra {palavra} NÃO está contida na descrição '{lista_descricao}'"
+    print(resultado)
+
+def ordenar_crescente(lista):
+    lista_nova = lista.copy()
+    tamanho = len(lista_nova)
+    for i in range(0, tamanho):
+        for j in range(0, i):
+            if lista_nova[i]['nome'] < lista_nova[j]['nome']:
+                temp = lista_nova[i]
+                lista_nova[i] = lista_nova[j]
+                lista_nova[j] = temp
+    print(lista_nova)
+
+def ordenar_descrescente(lista):
+    lista_nova = lista.copy()
+    tamanho = len(lista_nova)
+    for i in range(0, tamanho):
+        for j in range(0, i):
+            if lista_nova[i]['nome'] > lista_nova[j]['nome']:
+                temp = lista_nova[i]
+                lista_nova[i] = lista_nova[j]
+                lista_nova[j] = temp
+    print(lista_nova)
 
 cont = 0
 lista = []
